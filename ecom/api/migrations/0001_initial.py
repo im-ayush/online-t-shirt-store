08f1f5452a4bade31bb28210f72200d09fad6e9d
im-ayush <ayushmishra6768@gmail.com>
2021-07-15 02:00:54 +0530

    Added requirements.txt and api migrations

--- ecom/api/migrations/0001_initial.py
+++ ecom/api/migrations/0001_initial.py
@@ -1,17 +1,23 @@
 from django.db import migrations
 from api.user.models import CustomUser
 
+
 class Migration(migrations.Migration):
     def seed_data(apps, schema_editor):
-        user = CustomUser(name='ayush',
-                        email='ayush@gmail.com',
-                        is_staff=True,
-                        is_superuser=True,
-                        phone='97986465321',
-                        gender='Male',
-                        )
-        user.set_password("ayush")
+        user = CustomUser(name="ayush",
+                          email="ayush@xyz.com",
+                          is_staff=True,
+                          is_superuser=True,
+                          phone="9876543210",
+                          gender="Male"
+                          )
+        user.set_password("ayush12345")
         user.save()
 
-    dependencies=[]
-    operations=[migrations.RunPython(seed_data)]
+    dependencies = [
+
+    ]
+
+    operations = [
+        migrations.RunPython(seed_data),
+    ]
